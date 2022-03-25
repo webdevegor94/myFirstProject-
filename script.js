e5c135e41dcc3eafd2188c4c6feed978ae463a8b
Egor Gulid <egor-gulid@yandex.ru>
2022-03-26 00:25:56 +0300

    fix: проработка задания

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let title = "project_js";
-console.log('Тип данных значения переменной title: ' + typeof title);
 let screens = "Простые, Сложные, Интерактивные";
+let splits = screens.split(', ');
+let screenPrice = 256;
+let rollback = 20;
+let fullPrice = 15000;
+let adaptive = true;
+
+
 console.log(screens.length);
 console.log(screens.toLocaleLowerCase());
-let splits = screens.split(' ');
 console.log(splits);
-let screenPrice = 256 + " рублей";
-console.log(screenPrice);
-let rollback = 27;
-rollback = rollback / 100;
-console.log(rollback);
-let fullPrice = 20 + " долларов";
-console.log('Тип данных значения переменной fullPrice: ' + typeof fullPrice);
-console.log(fullPrice);
-console.log(fullPrice[0] * rollback);
-let adaptive = true;
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log("Сколько мне нужно сделать экранов чтобы заработать " + fullPrice / screenPrice);
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
+
 console.log('Тип данных значения переменной adaptive: ' + typeof adaptive);
-
-
+console.log('Тип данных значения переменной title: ' + typeof title);
+console.log('Тип данных значения переменной fullPrice: ' + typeof fullPrice);
 
 
 
