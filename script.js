e2242b4414ce436d7e13032e8465779e2dd5de85
Egor Gulid <egor-gulid@yandex.ru>
2022-03-25 20:56:23 +0300

    дополнил информацию по типам данных значений

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let title = "project_js";
-console.log(typeof title);
+console.log('Тип данных значения переменной title: ' + typeof title);
 let screens = "Простые, Сложные, Интерактивные";
 console.log(screens.length);
 console.log(screens.toLocaleLowerCase());
@@ -11,9 +11,11 @@ let rollback = 27;
 rollback = rollback / 100;
 console.log(rollback);
 let fullPrice = 20 + " долларов";
+console.log('Тип данных значения переменной fullPrice: ' + typeof fullPrice);
 console.log(fullPrice);
 console.log(fullPrice[0] * rollback);
 let adaptive = true;
+console.log('Тип данных значения переменной adaptive: ' + typeof adaptive);
 
 
 
