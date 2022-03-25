c120273c5ced7468c939ff2c7045fa7e1c476803
Egor Gulid <egor-gulid@yandex.ru>
2022-03-25 18:31:58 +0300

    присвоил значения переменным как написанно по заданию, использовал методы и свойства

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "project_js";
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 256;
+let rollback = 27;
+rollback = rollback / 100;
+console.log(typeof rollback);
+let fullPrice = 200000;
+console.log(typeof fullPrice);
+let adaptive = true;
 
 
-alert('Привет');
 
-console.log('My name Egor');
+
 
