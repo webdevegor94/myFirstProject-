422c18818ecd1ce216d7c8b60e31abd100126a88
Egor Gulid <egor-gulid@yandex.ru>
2022-04-19 23:42:24 +0300

    comlete lesson12

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict'
 const title = document.getElementsByTagName('h1')[0]
 const buttons = document.getElementsByClassName('handler_btn')
-const calcBtn = buttons[0]
-const resetBtn = buttons[1]
+const calcBtn = buttons[0] // рассчитать 
+const resetBtn = buttons[1] // сбросить 
 
 const plusBtn = document.querySelector('.screen-btn')
 const itemsPersent = document.querySelectorAll('.other-items.percent')
@@ -18,12 +18,13 @@ const servicePercentPriceInput = document.getElementsByClassName('total-input')[
 
 let screenElement = document.querySelectorAll('.screen')
 
+
 const appData = {
     title: '',
     screens: [],
     screenPrice: 0,
     adaptive: true,
-    rollback: 20,
+    rollback: 0,
     fullPrice: 0,
     rollbackPrice: 0,
     servicePercentPrice: 0,
@@ -35,9 +36,10 @@ const appData = {
 
     init: function () {
         appData.addTitle()
-        // appData.start()
         calcBtn.addEventListener('click', appData.start)
         plusBtn.addEventListener('click', appData.addScreenBlock)
+        rollbackInput.addEventListener('input', appData.getStepsInput)
+
     },
     addTitle: function () {
         document.title = title.textContent
@@ -45,34 +47,44 @@ const appData = {
 
     start: function () {
         appData.addScreens()
-        appData.addServices()
-        appData.addPrice()
-        // appData.getServicePercentPrices()
-        // appData.logger()
-        appData.showResult()
+        console.log(appData.screens)
+        if (appData.screens === 0) {
+            alert('Заполните данные')
+        } else {
+            appData.addServices()
+            appData.addPrice()
+            // appData.getServicePercentPrices()
+            appData.logger()
+            appData.showResult()
+        }
     },
 
     showResult: function () {
         screenPriceInput.value = appData.screenPrice
         screensCountInput.value = appData.servicePricesPercent + appData.servicePricesNumber
         fullPriceInput.value = appData.fullPrice
+        servicePercentPriceInput.value = appData.servicePercentPrice
     },
 
 
     addScreens: function () {
         let screenElement = document.querySelectorAll('.screen')
+        appData.screens = [] // чистим перед заполнением 
         screenElement.forEach(function (screen, index) {
             const select = screen.querySelector('select')
             const input = screen.querySelector('input')
             const selectName = select.options[select.selectedIndex].textContent
+            if (+select.value === 0) {
+                return
+            }
             appData.screens.push({
                 id: index,
                 name: selectName,
-                price: +select.value * +input.value
+                price: +select.value * +input.value,
+                count: screenPriceInput
             })
         })
-        screenElement = document.querySelectorAll('.screen').disabled = true;
-        console.log(appData.screens)
+
     },
 
     addServices: function () {
@@ -95,10 +107,9 @@ const appData = {
         })
     },
 
-
-
     addScreenBlock: function () {
-        const cloneScreen = screenElement[0].cloneNode(true)
+        screenElement = document.querySelectorAll('.screen')
+        const cloneScreen = screenElement[screenElement.length - 1].cloneNode(true)
         screenElement[screenElement.length - 1].after(cloneScreen)
     },
 
@@ -116,27 +127,17 @@ const appData = {
         }
 
         appData.fullPrice = +appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent
-    },
 
-    getRollbeckMessage: function (price) {
-        if (price >= 30000) {
-            return "Даем скидку 10%";
-        } else if (15000 >= price < 30000) {
-            return "Даем скидку 5%";
-        } else if (0 >= price < 15000) {
-            return "Скидка не предусмотрена";
-        } else {
-            return "Что-то пошло не так"
-        }
-    },
-
-    getRollbackPrice: function () {
         appData.rollbackPrice = Math.round(appData.fullPrice * (appData.rollback / 100))
-    },
 
-    getServicePercentPrices: function () {
         appData.servicePercentPrice = appData.fullPrice - appData.rollbackPrice
     },
+
+    getStepsInput: function (event) {
+        rollbackSpan.textContent = event.target.value + '%'
+        appData.rollback = event.target.value
+    },
+
     logger: function () {
 
         for (let key in appData) {
